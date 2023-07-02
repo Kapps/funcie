d7ff268d980b8e0f3d3f308ebabe864d54197812
Ogi <1259333+Kapps@users.noreply.github.com>
2023-07-01 20:05:19 -0400

    Work on getting host ready

--- cmd/client-bastion/bastion/host.go
+++ cmd/client-bastion/bastion/host.go
@@ -1,6 +1,12 @@
 package bastion
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Kapps/funcie/pkg/funcie"
+	"io"
+	"net/http"
+)
 
 // Host is the interface implemented by types that can host a Bastion server.
 type Host interface {
@@ -12,4 +18,45 @@ type Host interface {
 type bastionHost struct {
 	httpServer *http.Server
 	handler    Handler
+}
+
+// NewHost creates a new Host listening on the given address.
+func NewHost(address string, handler Handler) Host {
+	httpServer := &http.Server{
+		Addr: address,
+	}
+	return &bastionHost{
+		httpServer: httpServer,
+		handler:    handler,
+	}
+}
+
+func (h *bastionHost) setHandlers() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/register", h.register)
+	mux.HandleFunc("/unregister", h.unregister)
+	mux.HandleFunc("/dispatch", h.forwardRequest)
+}
+
+func (h *bastionHost) Listen() error {
+	if err := h.httpServer.ListenAndServe(); err != nil {
+		return fmt.Errorf("listen and serve: %w", err)
+	}
+}
+
+func (h *bastionHost) register(w http.ResponseWriter, r *http.Request) {
+	payloadBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+
+	var message funcie.Message
+	err = json.Unmarshal(payloadBytes, &message)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("invalid request"))
+		return
+	}
+
+	err = h.handler.Register(r.Context(), message)
 }
--- cmd/client-bastion/bastion/testutils_test.go
+++ cmd/client-bastion/bastion/testutils_test.go
@@ -2,15 +2,16 @@ package bastion_test
 
 import (
 	"github.com/Kapps/funcie/pkg/funcie"
+	"github.com/stretchr/testify/mock"
 	"reflect"
 	"testing"
 )
 
 func MessageComparer(t *testing.T, message *funcie.Message) interface{} {
-	return func(m *funcie.Message) bool {
+	return mock.MatchedBy(func(m *funcie.Message) bool {
 		cp := *m
 		cp.ID = message.ID
 		cp.Created = message.Created
 		return reflect.DeepEqual(&cp, message)
-	}
+	})
 }
