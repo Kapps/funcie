9e0679d6a164200b5708f99b025d5e0ac3e83e65
Ogi <1259333+Kapps@users.noreply.github.com>
2023-09-01 02:00:47 -0400

    Add terraform outputs and sample variables.

---
+++ deploy/outputs.tf
@@ -0,0 +1,16 @@
+
+output "server_bastion_lb_host" {
+  value = aws_lb.funcie_lb.dns_name
+}
+
+output "server_bastion_lb_arn" {
+  value = aws_lb.funcie_lb.arn
+}
+
+output "funcie_cluster_arn" {
+  value = aws_ecs_cluster.funcie_cluster.arn
+}
+
+output "server_bastion_sg_id" {
+  value = aws_security_group.server_bastion_sg.id
+}
---
+++ deploy/sample.tfvars
@@ -0,0 +1,4 @@
+subnet_ids        = ["priv-subnet-1", "priv-subnet-2"]
+public_subnet_ids = ["pub-subnet-1", "pub-subnet-2"]
+redis_host        = "127.0.0.1:6379"
+vpc_id            = "vpc-1"
