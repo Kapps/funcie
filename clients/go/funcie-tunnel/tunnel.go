274c24af17a4c70cb4dfbce6f5e3617bf7b69039
Ogi <1259333+Kapps@users.noreply.github.com>
2023-09-19 00:27:16 -0400

    Add a StartWithConfig to the Go client for better usability.

--- clients/go/funcie-tunnel/tunnel.go
+++ clients/go/funcie-tunnel/tunnel.go
@@ -10,17 +10,20 @@ import (
 // It is subject to the same restrictions as the handler for the underlying serverless function provider (such as lambda.Start).
 // See NewConfigFromEnvironment for the environment variables that are used.
 func Start(handler interface{}) {
+	config := NewConfigFromEnvironment()
+	StartWithConfig(*config, handler)
+}
+
+// StartWithConfig is a replacement to lambda.Start that configures the proxy from the given config.
+// See `Start` for more information.
+func StartWithConfig(config FuncieConfig, handler interface{}) {
 	if funcie.IsRunningWithLambda() {
 		// In a Lambda, we wait for the Lambda runtime to call the handler and forward that request to the bastion.
-		config := NewConfigFromEnvironment()
-
 		client := NewHTTPBastionClient(config.ServerBastionEndpoint)
 		proxy := NewLambdaFunctionProxy(config.ApplicationId, client, handler)
 		proxy.Start()
 	} else {
 		// Locally, we receive the request from the bastion.
-		config := NewConfigFromEnvironment()
-
 		receiver := NewLambdaBastionReceiver(config.ApplicationId, config.ListenAddress, config.ClientBastionEndpoint, handler)
 		receiver.Start()
 	}
