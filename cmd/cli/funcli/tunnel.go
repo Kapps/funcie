77d6c7ab205a504a80f3d439174ee5167c916a22
Ogi <1259333+Kapps@users.noreply.github.com>
2024-05-27 20:19:36 -0400

    Further moving to the SSM Manager Plugin

--- cmd/cli/funcli/tunnel.go
+++ cmd/cli/funcli/tunnel.go
@@ -11,10 +11,6 @@ import (
 	_ "github.com/aws/session-manager-plugin/src/sessionmanagerplugin/session/shellsession"
 	"github.com/google/uuid"
 	"net/http"
-	ws "nhooyr.io/websocket"
-
-	"io"
-	"net"
 )
 
 // TunnelOptions provides optional arguments for creating a network tunnel.
@@ -56,101 +52,4 @@ func (t *webhookTunnel) OpenTunnel(ctx context.Context, endpoint string, localPo
 	}
 
 	return ssmSession.Execute(log.Logger(false, ssmSession.ClientId))
-
-	/*listener, err := net.Listen("tcp", fmt.Sprintf(":%v", localPort))
-	if err != nil {
-		return fmt.Errorf("failed to dial local connection: %w", err)
-	}
-	defer func() { _ = listener.Close() }()
-
-	for {
-		fmt.Println("Waiting for connection on port", localPort)
-		localConn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error: failed to accept local connection", "error", err)
-			continue
-		}
-
-		fmt.Println("Accepted connection on port", localPort)
-
-		wsConn, _, err := ws.Dial(ctx, endpoint, &ws.DialOptions{
-			HTTPHeader: opts.Headers,
-			//Subprotocols: []string{"binary"},
-			CompressionMode: ws.CompressionDisabled,
-		})
-		if err != nil {
-			return fmt.Errorf("failed to dial WebSocket connection: %w", err)
-		}
-
-		fmt.Println("Connected to WebSocket")
-
-		go func() {
-			err := t.forward(ctx, localConn, wsConn)
-			if err != nil {
-				fmt.Println("Error: failed to forward connection", "error", err)
-			}
-			fmt.Println("Done forwarding connection")
-		}()
-	}*/
-}
-
-func (t *webhookTunnel) forward(ctx context.Context, localConn net.Conn, wsConn *ws.Conn) error {
-	cancelCtx, cancel := context.WithCancel(ctx)
-	//defer cancel()
-	//defer func() { _ = localConn.Close() }()
-	//defer func() { _ = wsConn.Close(ws.StatusGoingAway, "exiting") }()
-
-	errChan := make(chan error, 1)
-
-	go func() {
-		for {
-			fmt.Println("Reading from WebSocket")
-			_, message, err := wsConn.Read(cancelCtx)
-			if err != nil {
-				fmt.Println("Error reading from WebSocket: %w", err)
-				errChan <- fmt.Errorf("failed to read from WebSocket connection: %w", err)
-				cancel()
-				return
-			}
-
-			fmt.Println("Writing to local connection")
-			_, err = localConn.Write(message)
-			if err != nil {
-				fmt.Println("Error writing to local connection: %w", err)
-				errChan <- fmt.Errorf("failed to write to local connection: %w", err)
-				cancel()
-				return
-			}
-		}
-	}()
-
-	buffer := make([]byte, 20)
-	for {
-		if len(errChan) > 0 {
-			fmt.Println("Error channel has data")
-			return <-errChan
-		}
-
-		fmt.Println("Reading from local connection")
-		n, err := localConn.Read(buffer)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("Error reading from local connection: %w", err)
-				return fmt.Errorf("failed to read from local connection: %w", err)
-			}
-			fmt.Println("EOF from local connection")
-			return nil
-		}
-
-		fmt.Println("Read", n, "bytes from local connection")
-
-		fmt.Println("Writing to WebSocket")
-		err = wsConn.Write(ctx, ws.MessageBinary, buffer[:n])
-		if err != nil {
-			fmt.Println("Error writing to WebSocket: %w", err)
-			return fmt.Errorf("failed to write to WebSocket connection: %w", err)
-		}
-
-		fmt.Println("Wrote", n, "bytes to WebSocket")
-	}
 }
